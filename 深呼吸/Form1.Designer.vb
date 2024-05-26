718930d0ee37dacf09220e8a9bf18803bd70a15a
漢瓏 李 <hanlongli2004@gmail.com>
2024-05-26 21:39:13 +0800

    First_commit

---
+++ 深呼吸/Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.Transparent
+        Button1.BackgroundImage = My.Resources.Resources.Blue_Modern_Breakfast_Time__Flyer__0
+        Button1.BackgroundImageLayout = ImageLayout.Zoom
+        Button1.Cursor = Cursors.Hand
+        Button1.FlatAppearance.BorderSize = 0
+        Button1.FlatStyle = FlatStyle.Flat
+        Button1.ForeColor = Color.Transparent
+        Button1.Location = New Point(95, 246)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(243, 54)
+        Button1.TabIndex = 0
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.Transparent
+        Button2.BackgroundImage = My.Resources.Resources.Blue_Modern_Breakfast_Time__Flyer___2_
+        Button2.BackgroundImageLayout = ImageLayout.Zoom
+        Button2.Cursor = Cursors.Hand
+        Button2.FlatAppearance.BorderSize = 0
+        Button2.FlatStyle = FlatStyle.Flat
+        Button2.ForeColor = Color.Transparent
+        Button2.Location = New Point(95, 343)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(243, 54)
+        Button2.TabIndex = 1
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.Transparent
+        Button3.BackgroundImage = My.Resources.Resources.Blue_Modern_Breakfast_Time__Flyer___2__0
+        Button3.BackgroundImageLayout = ImageLayout.Zoom
+        Button3.Cursor = Cursors.Hand
+        Button3.FlatAppearance.BorderSize = 0
+        Button3.FlatStyle = FlatStyle.Flat
+        Button3.ForeColor = Color.Transparent
+        Button3.Location = New Point(95, 431)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(243, 54)
+        Button3.TabIndex = 2
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(9F, 19F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = My.Resources.Resources.Blue_Modern_Breakfast_Time__Flyer___1_
+        BackgroundImageLayout = ImageLayout.Stretch
+        ClientSize = New Size(449, 633)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        DoubleBuffered = True
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ 深呼吸/Form1.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Security.Cryptography
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Hide()
+        Dim From2 As New Form2()
+        From2.Show()
+
+    End Sub
+End Class
